38482018cb1fef8ae3a3c872d24385954b946b0e
karine <karineantunesfarias@gmail.com>
2021-04-21 00:12:35 -0300

    instantiation - instanciacao para colocar usuarios na base de dados

---
+++ src/main/java/com/example/workshopspringbootmongodb/config/Instantiation.java
@@ -0,0 +1,28 @@
+package com.example.workshopspringbootmongodb.config;
+
+import com.example.workshopspringbootmongodb.domain.User;
+import com.example.workshopspringbootmongodb.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        repository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        repository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
