80428ed80a22dd4929185021c601cc6af9b16d6d
karine <karineantunesfarias@gmail.com>
2021-04-21 01:35:20 -0300

    insercao de usuario com POST

--- src/main/java/com/example/workshopspringbootmongodb/services/UserService.java
+++ src/main/java/com/example/workshopspringbootmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.workshopspringbootmongodb.services;
 
 import com.example.workshopspringbootmongodb.domain.User;
+import com.example.workshopspringbootmongodb.dto.UserDTO;
 import com.example.workshopspringbootmongodb.repository.UserRepository;
 import com.example.workshopspringbootmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User user){
+        return repository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO){
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
